d9eb7bf2f8af80120b7872d6ec908b250bfa7e68
KANG WON YOUNG <wonyoung@gangwoncBookPro.Dlink>
2023-04-04 20:38:16 +0900

    퀴즈 에디터 유효성 검사

--- static/js/quizEditor.js
+++ static/js/quizEditor.js
@@ -1,7 +1,10 @@
 const quizContainer = $("#quiz-container");
 const addQuizBtn = $("#add-quiz");
 const uploadQuizBtn = $("#upload-quiz-btn");
+const addContentBtn = $("#add-multiple-choice-btn");
 const categorySelect = $("#category");
+
+let quizNumber = 1;
 
 const handleRemoveQuizBtnClick = (event) => {
   const result = confirm("해당 퀴즈를 삭제하시겠습니까?");
@@ -14,8 +17,6 @@ const handleRemoveQuizBtnClick = (event) => {
   quizElement.remove();
   hrElement.remove();
 };
-
-$(".quiz-remove-btn").on("click", handleRemoveQuizBtnClick);
 
 const handleAddQuizBtnClick = () => {
   const html = `
@@ -32,62 +33,38 @@ const handleAddQuizBtnClick = () => {
             rows="4"
         ></textarea>
         </div>
-        <div class="flex flex-col">
+        <div class="flex flex-col mb-2 multiple-choice">
         <div class="block text-gray-700 font-bold mb-2">
             보기를 입력하고 정답을 체크해주세요
         </div>
-        <label class="inline-flex items-center mt-3">
-            <input
-            type="radio"
-            class="form-radio text-indigo-600"
-            name="radio-group"
-            value="option1"
-            />
-            <input
-            type="text"
-            class="ml-3 block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            placeholder="1번 보기를 입력해주세요"
-            />
-        </label>
-        <label class="inline-flex items-center mt-3">
-            <input
-            type="radio"
-            class="form-radio text-indigo-600"
-            name="radio-group"
-            value="option2"
-            />
-            <input
-            type="text"
-            class="ml-3 block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            placeholder="2번 보기를 입력해주세요"
-            />
-        </label>
-        <label class="inline-flex items-center mt-3">
-            <input
-            type="radio"
-            class="form-radio text-indigo-600"
-            name="radio-group"
-            value="option3"
-            />
-            <input
-            type="text"
-            class="ml-3 block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            placeholder="3번 보기를 입력해주세요"
-            />
-        </label>
-        <label class="inline-flex items-center mt-3">
-            <input
-            type="radio"
-            class="form-radio text-indigo-600"
-            name="radio-group"
-            value="option3"
-            />
-            <input
-            type="text"
-            class="ml-3 block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            placeholder="4번 보기를 입력해주세요"
-            />
-        </label>
+          <div class="multiple-choice-container">
+            <label class="inline-flex items-center mt-3 w-full">
+                <input
+                type="radio"
+                class="form-radio text-indigo-600"
+                name="radio-group-${quizNumber}"
+                value="option1"
+                />
+                <input
+                type="text"
+                class="ml-3 block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="1번 보기를 입력해주세요"
+                />
+            </label>
+            <label class="inline-flex items-center mt-3 w-full">
+                <input
+                type="radio"
+                class="form-radio text-indigo-600"
+                name="radio-group-${quizNumber}"
+                value="option2"
+                />
+                <input
+                type="text"
+                class="ml-3 block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="2번 보기를 입력해주세요"
+                />
+            </label>
+          </div>
         </div>
         <div class="flex justify-end">
             <button
@@ -100,18 +77,25 @@ const handleAddQuizBtnClick = () => {
     </div>`;
 
   quizContainer.append(html);
+  quizNumber++;
 };
 
-const handleUploadQuizBtnClick = () => {
-  // 누락된 사항 체크
+// handleUploadQuizBtnClick 이벤트 내에서 사용되는 함수들
+
+const checkCategory = () => {
   const selectedValue = categorySelect.val();
   if (selectedValue === null) {
     alert("퀴즈의 카테고리를 선택해주세요");
     categorySelect.focus();
-    return;
+    return false;
   }
+  return selectedValue;
+};
 
+const checkTitles = () => {
   const textareas = $("textarea");
+
+  const titles = [];
   let checkTextarea = true;
   textareas.each(function () {
     if ($(this).val() === "") {
@@ -120,13 +104,119 @@ const handleUploadQuizBtnClick = () => {
       checkTextarea = false;
       return false;
     }
+
+    titles.push($(this).val());
   });
+
   if (checkTextarea === false) {
+    return false;
+  } else {
+    return titles;
+  }
+};
+
+const checkMultipleChoiceList = () => {
+  const multipleChoiceList = $(".multiple-choice");
+
+  isAllInputsFilled = true;
+  isRadioChecked = false;
+
+  multipleChoiceList.each(function () {
+    const radios = $(this).find("input[type='radio']");
+    const inputs = $(this).find("input[type='text']");
+
+    for (let i = 0; i < radios.length; i++) {
+      if (radios[i].checked === true) {
+        isRadioChecked = true;
+      }
+      if (inputs[i].value === "") {
+        isAllInputsFilled = false;
+      }
+    }
+
+    if (isRadioChecked === false) {
+      alert("정답이 선택되지 않은 문제가 있습니다.");
+      return false;
+    }
+
+    if (isAllInputsFilled === false) {
+      alert("입력되지 않은 보기가 있습니다.");
+      return false;
+    }
+  });
+  if (isRadioChecked === false) {
+    return false;
+  }
+
+  if (isAllInputsFilled === false) {
+    return false;
+  }
+  return multipleChoiceList;
+};
+
+const handleUploadQuizBtnClick = () => {
+  // 누락된 사항 체크
+  const category = checkCategory();
+  if (category === false) {
+    return;
+  }
+
+  const titles = checkTitles();
+  if (titles === false) {
+    return;
+  }
+
+  const multipleChoiceList = checkMultipleChoiceList();
+  console.log(multipleChoiceList);
+  if (multipleChoiceList === false) {
     return;
   }
 
   // 입력된 데이터를 request body 형식에 맞춰 변형
+
+  const problems = [];
+  for (let i = 0; i < titles.length; i++) {
+    const radios = $(multipleChoiceList[i]).find("input[type='radio']");
+    const inputs = $(multipleChoiceList[i]).find("input[type='text']");
+
+    let answer;
+    radios.each(function () {
+      if (this.checked) {
+        answer = this.value;
+      }
+    });
+
+    const content = [];
+    inputs.each(function () {
+      content.push(this.value);
+    });
+
+    const problem = {
+      title: titles[i],
+      category,
+      answer,
+      content,
+    };
+
+    problems.push(problem);
+  }
+
+  console.log(problems);
+
+  return;
+
   // POST Request 보내기
+
+  $.ajax({
+    url: "/api/problems",
+    data: { problems },
+    type: "POST",
+    dataType: "json",
+    success: function (response) {
+      alert("문제를 성공적으로 생성하였습니다");
+      location.href = "/";
+    },
+  });
 };
 
 addQuizBtn.on("click", handleAddQuizBtnClick);
